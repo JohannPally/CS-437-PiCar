a87af660f5269ce50979fea5cb3aecc0477fbf98
Johann Pally <pallyjohann@gmail.com>
2023-02-18 18:33:20 -0600

    moved lab folder and made control schema

---
+++ picar-4wd-master/picar_4wd/iot-labs/iot-lab-2/frontend_tutorial/control_lab2.py
@@ -0,0 +1,66 @@
+import picar_4wd as fc
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+class Control2:
+
+    def __init__(self):
+        fc.stop()
+
+        self.orientation = 'N'
+        self.mcodes = {b'w': "F", b's': "B", b'a': "L", b'd': "R"}
+        self.next_reference = {
+            'N': {'L':'W', 'F':'N', 'R':'E', 'B':'N'},
+            'E': {'L':'N', 'F':'E', 'R':'S', 'B':'E'},
+            'S': {'L':'E', 'F':'S', 'R':'W', 'B':'S'},
+            'W': {'L':'S', 'F':'W', 'R':'N', 'B':'W'}
+        }
+       
+        self.fuel = 10
+        return
+        
+    def move(self, code):
+        if self.fuel > 0:
+            movement = self.mcodes.get(code)
+
+            if movement is not None:
+                if movement == 'L':
+                    fc.stop()
+                    time.sleep(.1)
+                    fc.turn_left(50)
+                    time.sleep(1)
+                    fc.stop()
+                    time.sleep(.1)
+                    fc.forward(20)
+
+                elif movement == 'F':
+                    fc.forward(20)
+
+                elif movement == 'B':
+                    fc.stop()
+                    time.sleep(.1)
+                    fc.backward(20)
+
+                elif movement == 'R':
+                    fc.stop()
+                    time.sleep(.1)
+                    fc.turn_right(60)
+                    time.sleep(.9)
+                    fc.stop()
+                    time.sleep(.1)
+                    fc.forward(20)
+                
+                time.sleep(.7)
+                fc.stop()
+
+                self.fuel -= 1
+                self.orientation = self.next_reference[self.orientation][movement]
+            
+            else:
+                print('INVALID MOVEMENT CODE')
+        else:
+            print('Ran out of fuel')
+
+        return self.orientation, self.fuel
+    
--- picar-4wd-master/picar_4wd/iot-labs/iot-lab-2/frontend_tutorial/wifi_movement.py
+++ picar-4wd-master/picar_4wd/iot-labs/iot-lab-2/frontend_tutorial/wifi_movement.py
@@ -1,21 +1,25 @@
 import socket
+from control_lab2 import Control2
 
-HOST = "192.168.3.49" # IP address of your Raspberry PI
+HOST = "192.168.50.217" # IP address of your Raspberry PI
 PORT = 65432          # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
+    cntl = Control2()
 
     try:
         while 1:
             client, clientInfo = s.accept()
             print("server recv from: ", clientInfo)
             data = client.recv(1024)      # receive 1024 Bytes of message in binary format
+            
             if data != b"":
-                print(data)     
+                print(data)  
+                cntl.move(data)
                 client.sendall(data) # Echo back to client
     except: 
         print("Closing socket")
         client.close()
-        s.close()    
+        s.close()
